e8ad5e84a5215513d330d8f33791a139cd63c4f2
Антонова Анастасия <sabrina-19@mail.ru>
2023-07-07 21:49:10 +0300

    Проект 15. Добавление фронта и бэк7"

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -12,61 +12,93 @@ class Api {
   }
 
   getUserInfo() { //для получения данных о пользователе
+    const token = localStorage.getItem("token");
     return fetch(this._url + '/users/me', {
       method: 'GET',
-      headers: this._headers
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      }
     }).then((res) => this._getResponse(res));
   }
 
   getCardsInfo() { //для получения карточек с сервера
+    const token = localStorage.getItem("token");
     return fetch(this._url + '/cards', {
       method: 'GET',
-      headers: this._headers
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then((res) => this._getResponse(res));
   }
 
   setUserEdit(item) { //для изменения профиля
+    const token = localStorage.getItem("token");
     return fetch(this._url + '/users/me', {
       method: 'PATCH',
-      headers: this._headers,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify({ name: item.name, about: item.about })
     }).then((res) => this._getResponse(res));
   }
 
   setAvatarEdit({avatar}) { //для изменения аватара
+    const token = localStorage.getItem("token");
     return fetch(this._url + '/users/me/avatar', {
       method: 'PATCH',
-      headers: this._headers,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify({ avatar: avatar })
     }).then((res) => this._getResponse(res));
   }
 
   createNewCards ({name, link}) { //для добавления новой карточки
+    const token = localStorage.getItem("token");
     return fetch(this._url + '/cards', {
       method: 'POST',
-      headers: this._headers,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify({ name: name, link: link })
     }).then((res) => this._getResponse(res));
   }
 
   delCard(cardId){ //для удаления карточки
+    const token = localStorage.getItem("token");
     return fetch (`${this._url}/cards/${cardId}`, {
       method: 'DELETE',
-      headers: this._headers
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then((res) => this._getResponse(res));
   }
 
   _addLike(cardId) { //для добавления лайков
+    const token = localStorage.getItem("token");
     return fetch(`${this._url}/cards/${cardId}/likes`, {
       method: 'PUT',
-      headers: this._headers
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then((res) => this._getResponse(res));
   }
 
   _removeLike(cardId) { //для удаления лайков
+    const token = localStorage.getItem("token");
     return fetch(`${this._url}/cards/${cardId}/likes`, {
       method: 'DELETE',
-      headers: this._headers
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then((res) => this._getResponse(res));
   }
 
@@ -82,10 +114,10 @@ class Api {
 
 const api = new Api({
   url: 'https://aaa2208bek.nomoredomains.work',
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-    'Content-Type': 'application/json',
-    }
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem('token')}`,
+  //   'Content-Type': 'application/json',
+  //   }
   },
 );
 
